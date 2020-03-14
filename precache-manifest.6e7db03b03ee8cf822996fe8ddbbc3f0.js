b755e21b14b7bb94bf269dd345a456b1926ef401
Łukasz Zaparucha <lukasz.zaparucha@itmagination.com>
2020-03-14 04:41:41 +0100

    Updates

---
+++ precache-manifest.6e7db03b03ee8cf822996fe8ddbbc3f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "349b211903b3b085cf5cddc70a32b009",
+    "url": "/countries-api/index.html"
+  },
+  {
+    "revision": "327b54251a26aeb39c52",
+    "url": "/countries-api/static/css/main.1dbb321b.chunk.css"
+  },
+  {
+    "revision": "079a5773915e961cb883",
+    "url": "/countries-api/static/js/2.ae83fe3a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/countries-api/static/js/2.ae83fe3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327b54251a26aeb39c52",
+    "url": "/countries-api/static/js/main.bb13df1a.chunk.js"
+  },
+  {
+    "revision": "fa3f0a0d236bfbe6e258",
+    "url": "/countries-api/static/js/runtime-main.d7b1ca14.js"
+  }
+]);
